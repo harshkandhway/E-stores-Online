164c83633df34a3d9700659d9b076644135b28bf
Harsh Kandhway <harsh.kandhway.bca21@heritageit.edu.in>
2021-10-24 04:16:21 +0530

    password hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,12 +2,16 @@ const User = require('../models/user.js');
 
 const register= async(req, res)=>{
     try {
-        const {email} = req.body;
+        const {email,name,password} = req.body;
         const emailAlreadyExists = await User.findOne({email})
         if(emailAlreadyExists){
             return res.status(404).json({msg: `email already exist`})
         }
-        const user = await User.create(req.body);
+
+        const isFirstAccount = (await User.countDocuments({}))===0;
+        const role = isFirstAccount? 'admin':'user';
+
+        const user = await User.create({name, email, password, role});
         res.status(200).json({user})
     } catch (error) {
         res.status(500).json({ msgR: error.message })
