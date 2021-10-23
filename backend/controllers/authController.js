ac89263357d6aede676c09fffa874f9489c67029
Harsh Kandhway <harsh.kandhway.bca21@heritageit.edu.in>
2021-10-24 04:48:19 +0530

    setting jwt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user.js');
-
+const jwt = require('jsonwebtoken')
 const register= async(req, res)=>{
     try {
         const {email,name,password} = req.body;
@@ -10,9 +10,11 @@ const register= async(req, res)=>{
 
         const isFirstAccount = (await User.countDocuments({}))===0;
         const role = isFirstAccount? 'admin':'user';
-
         const user = await User.create({name, email, password, role});
-        res.status(200).json({user})
+        const tokenUser = {name: user.name, userId:user._id, role: user.role}
+        const token = jwt.sign(tokenUser,'jwtSecret',{expiresIn: '1d'})
+
+        res.status(200).json({user:tokenUser,token})
     } catch (error) {
         res.status(500).json({ msgR: error.message })
     }
